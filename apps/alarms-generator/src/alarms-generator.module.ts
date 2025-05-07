a4303f8345fcbda2347c854011e5ddf8f9a17c7e
markflerko <markflerko@gmail.com>
2025-05-07 12:17:20 +0300

    12. alarm service

---
+++ apps/alarms-generator/src/alarms-generator.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ALARMS_SERVICE } from 'apps/alarms-generator/src/constants';
+import { AlarmsGeneratorService } from './alarms-generator.service';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    ClientsModule.register([
+      {
+        name: ALARMS_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: process.env.NATS_URL,
+        },
+      },
+    ]),
+  ],
+  controllers: [],
+  providers: [AlarmsGeneratorService],
+})
+export class AlarmsGeneratorModule {}
---
+++ apps/alarms-service/src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AlarmsServiceModule } from './alarms-service.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AlarmsServiceModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: process.env.NATS_URL,
+        queue: 'alarms-service',
+      },
+    },
+    { inheritAppConfig: true },
+  );
+  await app.startAllMicroservices();
+
+  await app.listen(process.env.port ?? 3000);
+}
+bootstrap();
